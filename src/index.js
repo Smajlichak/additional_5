92921c92adbc36cfa5d6b796a495c7e69841c4c6
Smajlichak <smajlichak@yandex.ru>
2018-03-05 15:51:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
- module.exports = function check(str, bracketsConfig) {
-  var strl = str.length, configl = bracketsConfig.length;
-  for (var n = 0; n < configl; n++){
-    var count = 0;
-    for (var i = 0; i < strl; i++){
-      if (str[i] == bracketsConfig[n][0]){
-      count ++;
-      }
-      if (str[i] == bracketsConfig[n][1]){
-      count --;
-      if (count < 0){
-        break;
-      }
-      } 
-    }
-    if (count > 0 || count< 0){
-      return false;
-      break;
-    }
+  var strl = str.length, configl = bracketsConfig.length, openBrackets = [0];
+  if (strl%2){
+    return false;
   }
-  if (count == 0){
-    return true;
-  }
-}
-}
+  for (var i = 0; i < strl; i++){
+    for (var n = 0; n < configl; n++){
+      if (str[i] ==  bracketsConfig[n][0]){
+        openBrackets.push(str[i]);
+      }
+      if (str[i] ==  bracketsConfig[n][1]){
+
+ var last = openBrackets.pop();
+         if (bracketsConfig[n][0] == last){
+
+ 			continue;
+         }
+         else{
+           return false;
+         }
+       }
+     }
+     if (i == (strl - 1) && openBrackets.length == 1){
+       return true;
+   }
+   }
+ }
